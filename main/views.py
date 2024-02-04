41db386a11ad202cb9d350cd5b2cfb3c76f438cc
sergey <sergey.fomin.90@inbox.ru>
2024-02-04 13:22:40 +0400

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,18 +1,38 @@
 from importlib.resources import contents
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.defaulttags import lorem
 
 
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Главная страница магазина - HOME",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": False
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
     }
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About Page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        'text_on_page':'Текст о том что магазин классный и товар отличный',
+    }
+    return render(request, "main/about.html", context)
+
+def delivery(request):
+    context = {
+        "title": "Home - Доставка",
+        "content": "Доставка и оплата",
+        'text_on_page':'Мы доставляем вы оплачиваете',
+    }
+    return render(request, "main/delivery.html", context)
+
+def info(request):
+    context = {
+        "title": "Home - Информация",
+        "content": "Информация",
+        'text_on_page':'Контактная информация',
+    }
+    return render(request, "main/info.html", context)
+
