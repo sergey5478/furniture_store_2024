23135a7f83d5f9dc32872630cfd8edc99c410a2d
sergey <sergey.fomin.90@inbox.ru>
2024-02-11 21:51:59 +0400

    PostgreSQL data base connected & full text search

--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "django.contrib.postgres",
     "debug_toolbar",
 
     "main",
@@ -83,10 +83,20 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'home',
+        'USER': 'home',
+        'PASSWORD': "1234",
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from goods.models import Products
 
 
@@ -8,14 +8,21 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    q_objects = Q()
+    return Products().objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    for token in keywords:
-        # Добавляет условия через или, плюс еще или и тд(по описанию)
-        q_objects |= Q(description__icontains=token)
-        # Добавляет условия через или, плюс еще или и тд(по названию)
-        q_objects |= Q(name__icontains=token)
+    # return Products.objects.annotate(search=SearchVector("name", "description")).filter(search=query)
+    # return Products.objects.filter(description__search=query)
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    return Products.objects.filter(q_objects)
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     # Добавляет условия через или, плюс еще или и тд(по описанию)
+    #     q_objects |= Q(description__icontains=token)
+    #     # Добавляет условия через или, плюс еще или и тд(по названию)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
