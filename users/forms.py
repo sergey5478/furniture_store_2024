0a1c95c483d938e2dc708483d8c9394b07589b95
sergey <sergey.fomin.90@inbox.ru>
2024-02-17 22:24:48 +0400

    Registration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,16 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
 
-    username = forms.CharField()
-    password = forms.CharField()
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+        
+    # username = forms.CharField()
+    # password = forms.CharField()
 
     # username = forms.CharField(
     #     label = 'Имя',
@@ -22,13 +26,78 @@ class UserLoginForm(AuthenticationForm):
     #     label = 'Пароль',
     #     widget=forms.PasswordInput(
     #         attrs={
-    #             "autocomplate": "current-password",
+    #             "autocomplete": "current-password",
     #             "class": "form-control",
     #             "placeholder": "Введите ваш пароль",
     #         }
     #     ),
     # )
 
+class UserRegistrationForm(UserCreationForm):
+
     class Meta:
         model = User
-        fields = ["username", "password"]
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+    
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+# first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш email *youremail@example.com",
+    #         }
+    #     )
+    # )
+    # password1 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш пароль",
+    #         }
+    #     )
+    # )
+    # password2 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Поддтвердите ваш пароль",
+    #         }
+    #     )
+    # )
--- users/models.py
+++ users/models.py
@@ -1,7 +1,5 @@
-from email.mime import image
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 
 class User(AbstractUser):
     image = models.ImageField(upload_to="user_images", blank=True, null=True, verbose_name='Аватар')
