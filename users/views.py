6dc410b24d61b3ab10a95d9939cbd854b3ba4d73
sergey <sergey.fomin.90@inbox.ru>
2024-02-20 20:22:57 +0400

    Profile form, controler & notification are ready

--- users/views.py
+++ users/views.py
@@ -1,54 +1,62 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
+
 
 def login(request):
-    if request.method == 'POST':
-            form = UserLoginForm(data=request.POST)
-            if form.is_valid():
-                username = request.POST['username']
-                password = request.POST['password']
-                user = auth.authenticate(username=username, password=password)
-                if user:
-                    auth.login(request, user)
-                    return HttpResponseRedirect(reverse('main:index'))
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                messages.success(request, f'{username}, Вы вошли в аккаунт')
+                return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
 
-    context = {
-        'title': 'Home - Авторизация',
-        'form': form
-    }
-    return render(request, 'users/login.html', context)
+    context = {"title": "Home - Авторизация", "form": form}
+    return render(request, "users/login.html", context)
+
 
 def registration(request):
-    if request.method == 'POST':
-            form = UserRegistrationForm(data=request.POST)
-            if form.is_valid():
-                form.save()
-                user=form.instance
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f'{user.username}, Вы успешно зарегистрированны и вошли в аккаунт')
+            return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
 
-    context = {
-        'title': 'Home - Регистрация',
-        'form': form
-    }
-    return render(request, 'users/registration.html', context)
+    context = {"title": "Home - Регистрация", "form": form}
+    return render(request, "users/registration.html", context)
 
-
+@login_required
 def profile(request):
-    context = {
-        'title': 'Home - Кабинет'
-        
-    }
-    return render(request, 'users/profile.html', context)
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профайл успешно обновлен')
+            return HttpResponseRedirect(reverse("user:profile"))
+    else:
+        form = ProfileForm(instance=request.user)
+    context = {"title": "Home - Кабинет", "form": form}
+    return render(request, "users/profile.html", context)
 
+@login_required
 def logout(request):
+    messages.success(request, f'{request.user.username}, Вы вышли из аккаунта')
     auth.logout(request)
-    return redirect(reverse('main:index'))
+    return redirect(reverse("main:index"))
