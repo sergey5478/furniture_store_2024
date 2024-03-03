df1296f39f0215edeb1ebc174a2e91e578b5b6e6
sergey <sergey.fomin.90@inbox.ru>
2024-03-03 20:55:55 +0400

    Sessions - carts for anonymous user

--- carts/views.py
+++ carts/views.py
@@ -23,6 +23,18 @@ def cart_add(request):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1)
     
     user_cart = get_user_carts(request)
     # перводит в строку(ниже путь и по ключу берем всю информацию по корзинам пользователя)
@@ -36,8 +48,28 @@ def cart_add(request):
 
     return JsonResponse(response_data)
 
-def cart_change(request, product_slug):
-    ...
+def cart_change(request):
+    
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quaantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
