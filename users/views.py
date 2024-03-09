574725c8f586255dfdbae14f336c46e4a2a7eb3b
sergey <sergey.fomin.90@inbox.ru>
2024-03-09 14:27:52 +0400

    Final edits

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from carts.models import Cart
+from orders.models import Order, OrderItem
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
@@ -55,18 +57,31 @@ def registration(request):
 
 @login_required
 def profile(request):
-    if request.method == "POST":
-        form = ProfileForm(
-            data=request.POST, instance=request.user, files=request.FILES
-        )
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Профайл успешно обновлен')
-            return HttpResponseRedirect(reverse("user:profile"))
+            messages.success(request, "Профайл успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
-    context = {"title": "Home - Кабинет", "form": form}
-    return render(request, "users/profile.html", context)
+
+    orders = Order.objects.filter(user=request.user).prefetch_related(
+                Prefetch(
+                    "orderitem_set",
+                    queryset=OrderItem.objects.select_related("product"),
+                )
+            ).order_by("-id")
+        
+
+    context = {
+        'title': 'Home - Кабинет',
+        'form': form,
+        'orders': orders,
+    }
+    return render(request, 'users/profile.html', context)
+
+
 
 def users_cart(request):
     return render(request, "users/users_cart.html")
